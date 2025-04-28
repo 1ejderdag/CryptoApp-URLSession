fb28f42ed357c7bd207a9cd3b152074267cbc933
Ejder Dag <ejdrdag@gmail.com>
2025-04-28 13:48:17 +0300

    Initial Commit

---
+++ CryptoApp-URLSession/CryptoApp_URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp_URLSessionApp.swift
+//  CryptoApp-URLSession
+//
+//  Created by Ejder Dağ on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp_URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
